9810005aa610b17fb47b20290fa57509da5d4028
Sairaj <153721043+DevTerminator69@users.noreply.github.com>
2024-03-14 21:27:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const sourceUrl = '';
+const destUrl = '';
+
+async function transferData() {
+    try {
+
+        const sourceClient = await MongoClient.connect(sourceUrl, { useNewUrlParser: true });
+        const destClient = await MongoClient.connect(destUrl, { useNewUrlParser: true });
+
+        const sourceDB = sourceClient.db();
+        const destDB = destClient.db();
+
+        const collectionName = 'nightmodes';
+        const sourceCollection = sourceDB.collection(collectionName);
+        const destCollection = destDB.collection(collectionName);
+
+        console.log('Connected to source database');
+        const data = await sourceCollection.find().toArray();
+        console.log('Fetched data:', data);
+        
+        if (data.length === 0) {
+            console.log('No data to transfer.');
+            return;
+        }
+
+        await destCollection.insertMany(data);
+
+        console.log('Data transferred successfully');
+
+        sourceClient.close();
+        destClient.close();
+    } catch (err) {
+        console.error('Error transferring data:', err);
+    }
+}
+
+transferData();
